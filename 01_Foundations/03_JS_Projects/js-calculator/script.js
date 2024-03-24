b80b81da33774c5b675ee91227cac798afa9cebd
Josesx506 <josesomojola@gmail.com>
2024-03-24 01:50:45 -0700

    implemented feature for calculator power(^2) operator

--- 01_Foundations/03_JS_Projects/js-calculator/script.js
+++ 01_Foundations/03_JS_Projects/js-calculator/script.js
@@ -23,49 +23,122 @@ function calculator() {
     // Listen for button clicks across all buttons
     let inputButtons = document.querySelectorAll(".inp-btn");
     inputButtons.forEach(function(item) {
-        item.addEventListener("click", operate);
+        item.addEventListener("click", processInput);
     });
 }
 
-function operate(event) {
+function processInput(event) {
     let activeElement = event.target;
     let elementClasses = activeElement.classList;
-    let topScreenText = topScreen.textContent;
-    let bottomScreenText = bottomScreen.textContent;
+    let bottomScreenText = bottomScreen.textContent.replace("...", "");
 
     // Add numbers to screen
-    if (elementClasses.contains("numbers")) {
-        if (bottomScreenText === "0") {
-            bottomScreen.textContent = activeElement.textContent;
-        } else if (bottomScreenText !== "0" && bottomScreenText.length < maxLen) {
+    if (bottomScreenText.length < maxLen) {
+        if (elementClasses.contains("numbers")) {
+            if (bottomScreenText === "0") {
+                bottomScreen.textContent = activeElement.textContent;
+            } else if (bottomScreenText !== "0") {
+                bottomScreen.textContent = bottomScreenText + activeElement.textContent;
+            } 
+        }
+
+        // Adjust input for decimal input
+        if (elementClasses.contains("decimal") && !bottomScreenText.includes(".")) {
             bottomScreen.textContent = bottomScreenText + activeElement.textContent;
-        } 
-    }
+        }
 
-    // Adjust input for decimal input
-    if (elementClasses.contains("decimal") && !bottomScreenText.includes(".")) {
-        bottomScreen.textContent = bottomScreenText + activeElement.textContent;
-    }
+        // Adjust input for negative sign
+        if (elementClasses.contains("plus-minus") && !bottomScreenText.includes("-")) {
+            if (bottomScreenText !== "0"){
+                bottomScreen.textContent = "-" + bottomScreenText;
+            }
+        }
 
-    // Adjust input for negative sign
-    if (elementClasses.contains("plus-minus") && !bottomScreenText.includes("-")) {
-        if (bottomScreenText !== "0"){
-            bottomScreen.textContent = "-" + bottomScreenText;
+        if (elementClasses.contains("power")) {
+            let firstNum = "";
+            let sign = activeElement.textContent;
+            let secondNum = bottomScreen.textContent;
+            let result = operate(firstNum, secondNum, sign);
+            result = result.toString()
+            if (result.length < maxLen) {
+                bottomScreen.textContent = result;
+            } else {
+                bottomScreen.textContent = result.slice(0,5) + "...";
+                topScreen.textContent = "Max. Limit. Reset!!"
+                clearAllTrigger.textContent = "AC";
+                freezeScreen()
+                clearAllTrigger.disabled = false;
+                clearAllTrigger.style.backgroundColor = "red";
+                clearAllTrigger.style.color = "white";
+            }
+            
         }
     }
 
+    
+
     // Adjust operators
-    if (elementClasses.contains("special")) {
-        console.log(activeElement.textContent);
+    if (elementClasses.contains("operator") && !elementClasses.contains("power")) {
+        let topScreenText = topScreen.textContent;
+
+        if (topScreenText === "") {
+            topScreen.textContent = bottomScreenText + " " + activeElement.textContent;
+            bottomScreen.textContent = 0
+        } else {
+            let splitText = topScreen.textContent.split(" ");
+            let firstNum = splitText[0];
+            let sign = splitText[1];
+            let secondNum = bottomScreen.textContent;
+            let result = operate(firstNum, secondNum, sign);
+        }
     }
 
     // Clear screen listener
-    if (bottomScreen.textContent === "0") {
+    if (bottomScreen.textContent === "0" || bottomScreen.textContent.includes("...")) {
         clearAllTrigger.textContent = "AC";
     } else if (bottomScreen.textContent !== "0") {
         clearAllTrigger.textContent = "C";
     }
-    console.log(topScreenText);//"√x" "x2"
+    console.log(elementClasses);//"√x" "x2"
+}
+
+
+
+
+function operate(num1, num2, symbol) {
+    console.log(num1, num2, symbol);
+    if (num1) {
+        num1 = parseFloat(num1);
+    }
+    if (num2) {
+        num2 = parseFloat(num2);
+    }
+
+    let solution = 0;
+
+    // if (symbol === "x2") {
+    //     solution = Math.pow(num2, 2);
+    // }
+    // your code goes here
+    switch (symbol) {
+        case "+":
+            solution = 25636;
+            break;
+        case "-":
+            solution = 35256;
+            break;
+        case "÷":
+            solution = 41496;
+            break;
+        case "x":
+            solution = 59124;
+            break;
+        case "x2":
+            solution = Math.pow(num2, 2);
+            break;
+    }
+    let round = Math.round(solution * 1000) / 1000;
+    return round;
 }
 
 
@@ -88,7 +161,27 @@ function clearScreen(event) {
     if (activeElement.textContent === "C") {
         bottomScreen.textContent = 0;
     } else if(activeElement.textContent === "AC") {
+        unfreezeScreen()
         topScreen.textContent = "";
         bottomScreen.textContent = 0;
     } 
+}
+
+
+function freezeScreen() {
+    // Listen for button clicks across all buttons
+    let inputButtons = document.querySelectorAll(".inp-btn");
+    inputButtons.forEach(function(item) {
+        item.disabled = true;
+    });
+}
+
+function unfreezeScreen() {
+    clearAllTrigger.style.backgroundColor = "#f0f0f0";
+    clearAllTrigger.style.color = "black";
+    // Listen for button clicks across all buttons
+    let inputButtons = document.querySelectorAll(".inp-btn");
+    inputButtons.forEach(function(item) {
+        item.disabled = false;
+    });
 }

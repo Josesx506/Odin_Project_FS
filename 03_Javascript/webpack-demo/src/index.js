ef80150919ef7a52912208625e2464b2ceb3dfb0
Josesx506 <josesomojola@gmail.com>
2024-05-15 21:41:56 -0400

    webpack example

--- 03_Javascript/webpack-demo/src/index.js
+++ 03_Javascript/webpack-demo/src/index.js
@@ -1,16 +1,27 @@
-import _ from 'lodash';
+import _ from "lodash";
 // import your function
-import myName from './myName';
+import myName from "./myName";
+import "./style.css";
+import Icon from './icon.png';
 
 function component() {
-    const element = document.createElement('div');
+    const element = document.createElement("div");
   
     // Lodash, now imported by this script
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.innerHTML = _.join(["Hello", "webpack"], " ");
 
     // Update the elements text content with the custom function
-    element.textContent = myName('Cody');
-  
+    const myText = document.createElement("p");
+    myText.textContent = myName("Cody");
+
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myText);
+    element.appendChild(myIcon);
+    element.classList.add("hello");
+
     return element;
   }
   
--- 03_Javascript/webpack-demo/webpack.config.js
+++ 03_Javascript/webpack-demo/webpack.config.js
@@ -1,9 +1,40 @@
+// webpack.config.js
 const path = require('path');
+
+// Use a html template
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// plugins: [
+//     new HtmlWebpackPlugin({
+//         template: './src/template.html',
+//     }),
+// ],
+// ...
+
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
+    // filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      // Including css files
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Including images
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // Including fonts
+      {
+        test: /\.(woff|woff2|eot|truetype|opentype)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };

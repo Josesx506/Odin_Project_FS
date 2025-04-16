e48e28d3c536fa7349cb90dbeeb4f3beccd1e2e2
Josesx506 <45031412+Josesx506@users.noreply.github.com>
2025-04-16 06:01:51 -0700

    Completed integration for creating new forms

---
+++ 07_Node/16_blog_api/blog_panel/src/components/posts/CreatePost.jsx
@@ -0,0 +1,77 @@
+'use client';
+
+import styles from '@/styles/post.module.css';
+import { createPostHandler, editPostHandler } from '@/utils/postHandlers';
+import Form from 'next/form';
+import { useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { ContainedButton } from '../Buttons';
+
+export default function CreatePost() {
+  const blogTitleRef = useRef("");
+  const blogBodyRef = useRef("");
+  const blogPublishRef = useRef(false);
+  const [empty, setEmpty] = useState(false);
+  const [postId, setPostId] = useState(null);
+  const [isSaving, setIsSaving] = useState(false);
+
+  function clearForm(e) {
+    e.preventDefault();
+    blogTitleRef.current.value = '';
+    blogBodyRef.current.value = '';
+  }
+
+  async function saveBlog(e) {
+    e.preventDefault();
+    setIsSaving(true);
+
+    try {
+      if (!postId) {
+        // Create new post
+        await createPostHandler({
+          titleRef: blogTitleRef,
+          bodyRef: blogBodyRef,
+          publishedRef: blogPublishRef,
+          recordPostId: setPostId
+        })
+      } else {
+        await editPostHandler({
+          titleRef: blogTitleRef,
+          bodyRef: blogBodyRef,
+          publishedRef: blogPublishRef,
+          postId
+        })
+      }
+    } catch (err) {
+      toast.error(err.message || "Failed to save post");
+    } finally {
+      setIsSaving(false);
+    }
+  }
+
+  return (
+    <div className={styles.createPost}>
+      <Form className={styles.postForm} onSubmit={saveBlog}>
+        <div className={styles.postTextContainers}>
+          <label htmlFor='title'></label>
+          <input ref={blogTitleRef} id='title'
+            placeholder='Enter your title...' name='title'></input>
+        </div>
+        <div className={styles.postTextContainers}>
+          <label htmlFor='body'></label>
+          <textarea ref={blogBodyRef} id='body' placeholder='Document your thoughts...'
+            rows={20} name='body'></textarea>
+        </div>
+        <div className={styles.postPublish} >
+          <label htmlFor='published'>Publish this draft?</label>
+          <input ref={blogPublishRef} id='published' type='checkbox' name='published'></input>
+        </div>
+        <div className={styles.actionBtns}>
+          <ContainedButton onClick={clearForm} backgroundColor='lightgray' color='aliceblue'>Clear</ContainedButton>
+          <ContainedButton>Save</ContainedButton>
+        </div>
+      </Form>
+      <em><b style={{ color: 'tomato' }}>Note:</b> Populate the title and body values for valid submissions. Special characters are escaped.</em>
+    </div>
+  )
+}
--- 07_Node/16_blog_api/blog_panel/src/utils/postHandlers.js
+++ 07_Node/16_blog_api/blog_panel/src/utils/postHandlers.js
@@ -4,6 +4,51 @@ import { toast } from 'react-hot-toast';
 import { handleCreatePost, handleDeletePost, handleEditPost } from './postActions';
 
 
+async function createPostHandler({
+  titleRef, bodyRef, publishedRef, recordPostId
+}) {
+  const title = titleRef.current.value.trim();
+  const body = bodyRef.current.value.trim();
+  const published = publishedRef.current.checked;
+
+  if (!title || !body) {
+    toast.error('Title and body required');
+    return;
+  }
+  const post = { title, body, published };
+
+  try {
+    const newPost = await handleCreatePost({ body: post });
+    toast.success('Post saved');
+    recordPostId(newPost.id)
+  } catch(err) {
+    toast.error(err.message || 'Create Post Failed');
+  }
+}
+
+
+async function editPostHandler({
+  titleRef, bodyRef, publishedRef, postId
+}) {
+  const title = titleRef.current.value.trim();
+  const body = bodyRef.current.value.trim();
+  const published = publishedRef.current.checked;
+
+  if (!title || !body) {
+    toast.error('Title and body required');
+    return;
+  }
+  const post = { title, body, published };
+
+  try {
+    const updatedPost = await handleEditPost({ postId, body: post });
+    toast.success('Post updated');
+  } catch(err) {
+    toast.error(err.message || 'Post Edit Failed');
+  }
+}
+
+
 async function deletePostDetails({
   postId 
 }) {
@@ -29,4 +74,5 @@ async function deletePostThumbnailHandler({
 }
 
 
-export { deletePostDetails, deletePostThumbnailHandler }
+export { createPostHandler, deletePostDetails, deletePostThumbnailHandler, editPostHandler };
+
